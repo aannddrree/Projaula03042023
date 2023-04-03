64951c38c74cc805a883b1c300d1d62e9fb98f95
= <andre.silva@hubdoctor.com.br>
2023-04-03 19:43:19 -0300

    Adicionar arquivos de projeto.

---
+++ Projaula03042023/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Projaula03042023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValorA = new System.Windows.Forms.Label();
+            this.txtValorA = new System.Windows.Forms.TextBox();
+            this.txtValorB = new System.Windows.Forms.TextBox();
+            this.lblValorB = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblValorA
+            // 
+            this.lblValorA.AutoSize = true;
+            this.lblValorA.Location = new System.Drawing.Point(43, 62);
+            this.lblValorA.Name = "lblValorA";
+            this.lblValorA.Size = new System.Drawing.Size(54, 16);
+            this.lblValorA.TabIndex = 0;
+            this.lblValorA.Text = "Valor A:";
+            // 
+            // txtValorA
+            // 
+            this.txtValorA.Location = new System.Drawing.Point(46, 81);
+            this.txtValorA.Name = "txtValorA";
+            this.txtValorA.Size = new System.Drawing.Size(258, 22);
+            this.txtValorA.TabIndex = 1;
+            // 
+            // txtValorB
+            // 
+            this.txtValorB.Location = new System.Drawing.Point(46, 142);
+            this.txtValorB.Name = "txtValorB";
+            this.txtValorB.Size = new System.Drawing.Size(258, 22);
+            this.txtValorB.TabIndex = 3;
+            // 
+            // lblValorB
+            // 
+            this.lblValorB.AutoSize = true;
+            this.lblValorB.Location = new System.Drawing.Point(43, 123);
+            this.lblValorB.Name = "lblValorB";
+            this.lblValorB.Size = new System.Drawing.Size(54, 16);
+            this.lblValorB.TabIndex = 2;
+            this.lblValorB.Text = "Valor B:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(229, 192);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 266);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtValorB);
+            this.Controls.Add(this.lblValorB);
+            this.Controls.Add(this.txtValorA);
+            this.Controls.Add(this.lblValorA);
+            this.Name = "Form1";
+            this.Text = "Calculo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValorA;
+        private System.Windows.Forms.TextBox txtValorA;
+        private System.Windows.Forms.TextBox txtValorB;
+        private System.Windows.Forms.Label lblValorB;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Projaula03042023/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projaula03042023
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtValorA.Text);
+            int b = int.Parse(txtValorB.Text);
+
+            MessageBox.Show("A Soma é: " + (a + b));
+
+        }
+    }
+}
